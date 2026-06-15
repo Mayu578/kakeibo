44e1bbf81edbb3b7c83642199d1f6036a33ee66a
Mayu Saitoh <mayu@MayunoMacBook-Air.local>
2026-06-15 23:59:43 +0900

    database.phpのsslmodeをrequireに修正

--- config/database.php
+++ config/database.php
@@ -18,7 +18,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require', // 👈 正しい設定に直しました！
+            'sslmode' => 'require', // 👈 正しい設定（require）に直しました！
         ],
 
     ],
@@ -35,7 +35,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'database' => env('DB_DATABASE', '0'),
         ],
     ],
 
