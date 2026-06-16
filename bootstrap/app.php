8115933e5f8ce069facd7a2d943b094778a3dfbf
Mayu Saitoh <mayu@MayunoMacBook-Air.local>
2026-06-16 19:34:47 +0900

    Fix: HTTPS proxy trust and force scheme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // ここでプロキシを信頼するように設定
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
